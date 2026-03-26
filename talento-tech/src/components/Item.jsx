45b67b6f7282b4c9fcea5f2f259d08e1892ae037
valverdecristian <crizthian2010@gmail.com>
2026-03-26 14:28:41 -0300

    Clase #4: useState y Eventos

--- talento-tech/src/components/Item.jsx
+++ talento-tech/src/components/Item.jsx
@@ -1,12 +1,38 @@
 import styles from './ItemListContainer.module.css'
+import { useState } from 'react';
 
 const Item = ({nombre, precio, stock}) => {
+
+  const [cantidad, setCantidad] = useState(0);
+
+  const incrementar = () => {
+    if (cantidad < stock) {
+      setCantidad(cantidad + 1)
+    }
+  }
+
+  const decrementar = () => {
+    if (cantidad > 1) {
+      setCantidad(cantidad - 1)
+    }
+  }
+  
+  const agregarAlCarrito = () => {
+    alert(`¡Agregaste ${cantidad} unidades de ${nombre}  al carrito.`);
+  }
+
   return (
     <div className={styles.productos}>
-      <h3 className={styles.subtitulo}>{nombre}</h3>
+      <h3>{nombre}</h3>
       <p>Precio: ${precio}</p>
       <p>Stock disponible: {stock}</p>
-      <button>Comprar</button>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent:'center', margin: '10px 0' }}>
+        <button onClick={decrementar}>-</button>
+        <p style={{ margin: '0 10px' }}>{cantidad}</p>
+        <button onClick={incrementar}>+</button>
+      </div>
+
+      <button onClick={agregarAlCarrito}>Agregar al Carrito</button>
     </div>
   )
 }
